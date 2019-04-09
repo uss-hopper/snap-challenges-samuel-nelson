db80cd4c71efbcb79ee45d72c1d80d7bbc258ee5
Samuel Nelson <snelson54@cnm.edu>
2019-04-09 09:00:23 -0600

    Added operation and concatenation functions

---
+++ snap_04_09/script.js
@@ -0,0 +1,25 @@
+//function 1
+
+function multipyNumbers() {
+	var x = 5;
+	var y = 12;
+	var z = x * y;
+}
+
+console.log(multipyNumbers());
+
+//or
+
+console.log(Math.imul(5, 12));
+
+
+//function 2
+
+function concatString() {
+	function printConcatString() {
+		var string1 = "Hello";
+		var string2 = "world!";
+		return = string1.concat(string2);
+	}
+	console.log(printConcatString());
+}
