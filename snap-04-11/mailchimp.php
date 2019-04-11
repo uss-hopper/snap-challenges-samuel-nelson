915fbe6f3c1ce16b8da787b86854c7d083853ad9
Samuel Nelson <snelson54@cnm.edu>
2019-04-11 08:58:57 -0600

    Added title.:

--- snap-04-11/mailchimp.php
+++ snap-04-11/mailchimp.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Title</title>
+		<title>User story</title>
 	</head>
 	<body>
 		<p>As a casual user, I can insert images into a campaign.</p>
