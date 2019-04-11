c77ba8454a88b0a8aa446cfb0010b0f224c69c5c
Samuel Nelson <snelson54@cnm.edu>
2019-04-11 08:55:15 -0600

    Added ERD, user story.

---
+++ snap-04-11/erd.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Title</title>
+	</head>
+	<body>
+		<img src="shipping-address.png" alt="erd model">
+	</body>
+</html>
---
+++ snap-04-11/mailchimp.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Title</title>
+	</head>
+	<body>
+		<p>As a casual user, I can insert images into a campaign.</p>
+	</body>
+</html>
