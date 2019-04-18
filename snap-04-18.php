0251980d83791754792b92c30f2ef6ad663617d2
Samuel Nelson <snelson54@cnm.edu>
2019-04-18 08:43:10 -0600

    added snap-04-18 challenge

---
+++ snap-04-18.php
@@ -0,0 +1,24 @@
+Given weightPounds:
+Set intermediaryVariable to weightPounds 3;
+Set weightGrams to intermediaryVariable  3 * 453.592;
+return weightGrams;
+
+Given weightPounds:
+Set intermediaryVariable to weightPounds "lbs.";
+Set weightGrams to intermediaryVariable  "grams";
+return weightGrams;
+
+
+num = 6
+sum = 0
+given numberOne equals = 1
+
+for{ (numbers >= 8)
+	if {
+		numberOne / 8 modulo === 0
+	add numberOne to Sum
+	}
+	print sum
+}
+
+
