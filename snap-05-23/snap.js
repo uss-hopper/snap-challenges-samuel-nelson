c2529e5ac2d9fe010e266a2d66206126943fc32c
Samuel Nelson <snelson54@cnm.edu>
2019-05-23 08:50:06 -0600

    Attempted solution

---
+++ snap-05-23/snap.js
@@ -0,0 +1,7 @@
+let words = ["Ensign Babyface! Your shields were failing, sir. This should be interesting. Your head is not an artifact! What's a knock-out like you doing in a computer-generated gin joint like this? Why don't we just give everybody a promotion and call it a night - 'Commander'? But the probability of making a six is no greater than that of rolling a seven. What? We're not at all alike! I recommend you don't fire until you're within 40,000 kilometers. Wouldn't that bring about chaos? Flair is what marks the difference between artistry and mere competence."];
+
+const result = words.filter(word => word.startsWith ("e"));
+
+console.log(result);
+
+;
