906c89206278de2c467a88660b1b7d569194b28f
Samuel Nelson <snelson54@cnm.edu>
2019-05-23 09:12:44 -0600

    Added solution

--- snap-05-23/snap.js
+++ snap-05-23/snap.js
@@ -1,7 +1,6 @@
-let words = ["Ensign Babyface! Your shields were failing, sir. This should be interesting. Your head is not an artifact! What's a knock-out like you doing in a computer-generated gin joint like this? Why don't we just give everybody a promotion and call it a night - 'Commander'? But the probability of making a six is no greater than that of rolling a seven. What? We're not at all alike! I recommend you don't fire until you're within 40,000 kilometers. Wouldn't that bring about chaos? Flair is what marks the difference between artistry and mere competence."];
+// Using the array filter prototype filter out all the words that start with the letter e.
 
-const result = words.filter(word => word.startsWith ("e"));
+const ipsum = "Ensign Babyface! Your shields were failing, sir. This should be interesting. Your head is not an artifact! What's a knock-out like you doing in a computer-generated gin joint like this? Why don't we just give everybody a promotion and call it a night - 'Commander'? But the probability of making a six is no greater than that of rolling a seven. What? We're not at all alike! I recommend you don't fire until you're within 40,000 kilometers. Wouldn't that bring about chaos? Flair is what marks the difference between artistry and mere competence.";
 
-console.log(result);
-
-;
+const newIpsum = ipsum.split(" ").filter(word => !word.toLowerCase().startsWith("e")).join(" ");
+console.log(newIpsum);
